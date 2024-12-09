2ba0643e5c811c8412d5fd256ec4cf683b47f2d9
guilherme <guilherme@DESKTOP-8CIV3A3>
2024-12-08 22:25:55 -0300

    Adicionar arquivos de projeto.

---
+++ descobrir chaves/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+	//espectativa é usar recursividade com uma matriz com recursividade para solucionar o problema
+	verificachave(4x3x3x4xcxfx6x9xfx3xaxcx5x0x4xbxbx7x2x6x8x7x8xax4x0x8x3x3x3x7x3x);
+
+}
+int verificachave()
+//fazer função que verifique a chave a tual na ultima linha da matriz
+//fazer função matriz( chave [a][x][b][x][c][x]... sendo tudo que for x tem que ser analizado
+// fazer funcção que colete a chave privada e separe ela em variaveis descobertas e nao descobertas
+// 
+// chave privada -  4x3x3x4xcxfx6x9xfx3xaxcx5x0x4xbxbx7x2x6x8x7x8xax4x0x8x3x3x3x7x3x  -
